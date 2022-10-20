7763db9c23c3c6a3e890345d7691a342f0b95fcb
Aminat Ahmed <oahmedaminat@gmail.com>
2022-10-20 14:32:33 +0100

    add user.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import User from "./User";
 
 const Home = () => {
   return (
     <div>
       <h1>This is the homepage</h1>
-      <NavLink
-        className="link"
-        style={({ isActive }) => ({
-          color: isActive ? "green" : "",
-        })}
-        to="/about"
-      >
-        <p>About</p>
-      </NavLink>
-      <NavLink
-        className="link"
-        style={({ isActive }) => ({
-          color: isActive ? "green" : "",
-        })}
-        to="/"
-      >
-        <p>Home</p>
-      </NavLink>
+      <div className="nav">
+        <NavLink
+          className="link"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "",
+          })}
+          to="/about"
+        >
+          <p>About</p>
+        </NavLink>
+        <NavLink
+          className="link"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "",
+          })}
+          to="/"
+        >
+          <p>Home</p>
+        </NavLink>
+      </div>
+
+      <User />
     </div>
   );
 };
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from "react";
+import image from "../assets/Aminat.jpg";
+
+function UserProfile() {
+  return (
+    <div className="card">
+      <p>Username: Aminat</p>
+      <p>Email : aminatobi@gmail.com</p>
+      <p>Address: Ikorodu, Lagos</p>
+      <p>Phone: 0811223355</p>
+      <p>
+        Image:
+        <img
+          src={image}
+          alt="UserImage"
+          style={{ width: "200px;", height: "200px" }}
+        />
+      </p>
+    </div>
+  );
+}
 
 const User = () => {
   return (
     <div>
-        {/* name 
-            email
-            address
-            phone number
-            image at the center
-        */}
+      <UserProfile />
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
