dc596e5c239377951c938ab031a63b30516b5a78
Aminat Ahmed <oahmedaminat@gmail.com>
2022-10-21 13:25:49 +0100

    Updates

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,22 +1,45 @@
-import React from "react";
-import image from "../assets/Aminat.jpg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function UserProfile() {
+
+  const [details, setDetails] = useState([]);
+
+  const getData = async () => {
+    const {data} = await axios.get("https://randomuser.me/api/?results=5");
+    console.log(data.results);
+
+    const details = data.results
+    setDetails(details)
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="card">
-      <p>Username: Aminat</p>
-      <p>Email : aminatobi@gmail.com</p>
-      <p>Address: Ikorodu, Lagos</p>
-      <p>Phone: 0811223355</p>
+    
+      details.map((values) => {
+        return(
+          <div className="card" key={values.id}>
+      <p>Username: {values.name?.title}. {values.name?.first}</p>
+      <p>Email : {values.email}</p>
+      <p>Address: {values.location?.city}</p>
+      <p>Phone: {values.phone}</p>
       <p>
         Image:
         <img
-          src={image}
+          src={values.picture?.medium}
           alt="UserImage"
-          style={{ width: "200px;", height: "200px" }}
+          style={{ width: "200px;", height: "200px", borderRadius: '50%' }}
         />
       </p>
     </div>
+        )
+        
+      })
+    
+    
   );
 }
 
