fb5f73eaadd86e4cb6394b2c22d9ea6830385cc8
Chisom Odo <odochisomesther@gmail.com>
2022-10-26 08:07:11 +0100

    Update About.js
    reduced the lorem ipsum text

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,17 +16,7 @@ const About = () => {
           <div className="about-info">
             <h2>About Us</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh non
-              et orci ultrices dictum aliquam pretium cras. Sollicitudin cursus
-              penatibus ipsum sit praesent sem erat. Massa leo placerat viverra
-              viverra vulputate neque orci. Aliquam elementum mauris,
-              pellentesque ullamcorper vitae. Quis rutrum dui vulputate
-              facilisis. Proin faucibus turpis suspendisse fermentum odio etiam
-              velit lectus. Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit. Nibh non et orci ultrices dictum aliquam pretium cras.
-              Sollicitudin cursus penatibus ipsum sit praesent sem erat. Massa
-              leo placerat viverra viverra vulputate neque orci. Aliquam
-              elementum mauris, pellentesque ullamcorper vitae.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh non et orci ultrices dictum aliquam pretium cras. Sollicitudin cursus penatibus ipsum sit praesent sem erat. Massa leo placerat viverra viverra vulputate neque orci. Aliquam elementum mauris, pellentesque ullamcorper vitae. Quis rutrum dui vulputate facilisis. Proin faucibus turpis suspendisse fermentum odio etiam velit lectus 
             </p>
           </div>
         </div>
